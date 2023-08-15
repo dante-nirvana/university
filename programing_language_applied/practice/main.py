95c13770b2edd3b6e66986f11452295922daa86a
DanteGDV <dante.atom@gmail.com>
2023-08-15 15:09:43 -0300

    practice 1 part 2

--- programing_language_applied/practice/main.py
+++ programing_language_applied/practice/main.py
@@ -27,13 +27,38 @@ screen.blit(source=p1_surface, dest=p1_rect)
 
 # updating the screen
 pygame.display.flip()
+
+# creating a clock for our game
+clock = pygame.time.Clock()
+
+# load and play soundtrack
+pygame.mixer_music.load('./audio_clip/bg_track.mp3')
+pygame.mixer_music.play(-1)
+pygame.mixer_music.set_volume(0.5)
+
 print('Main loop initialized')
 while running:
+    clock.tick(120)
+
+    #drawing and updating the screen each loop
+    screen.blit(source=bg_surface, dest=bg_rect)
+    screen.blit(source=p1_surface, dest=p1_rect)
+    pygame.display.flip()
+
+    # looking for the event that closes the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print('Main loop ended')
             running = False
+
     pressed_key = pygame.key.get_pressed()
     if pressed_key[pygame.K_w]:
-        pass
+        p1_rect.centery -= 1
+    if pressed_key[pygame.K_s]:
+        p1_rect.centery += 1
+    if pressed_key[pygame.K_a]:
+        p1_rect.centerx -= 1
+    if pressed_key[pygame.K_d]:
+        p1_rect.centerx += 1
+    
 print('End')
